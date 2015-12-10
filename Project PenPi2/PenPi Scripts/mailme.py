a02473ead78e6c77403eb493004610631a2a46bf
Muldoser <michielmldr@gmail.com>
2015-12-10 16:30:34 +0100

    Algemene progress

--- Project PenPi2/PenPi Scripts/mailme.py
+++ Project PenPi2/PenPi Scripts/mailme.py
@@ -8,7 +8,7 @@ import re
 def getPublicIp():
         data=str(urlopen('http://checkip.dyndns.com/').read())
         return re.compile(r'Address: (\d+\.\d+\.\d+\.\d+)').search(data).group(1)
-to = 's079157@ap.be'
+to = 'penpi.infsec@gmail.com'
 today = datetime.date.today()
 arg='ip route list'
 p=subprocess.Popen(arg,shell=True,stdout=subprocess.PIPE)
@@ -19,13 +19,13 @@ public_ip = getPublicIp()
 my_ip = 'Your RPi ip is %s / %s' %  (ipaddr, public_ip)
 msg = MIMEText(my_ip)
 msg['Subject'] = 'IP For RaspberryPi on %s' % today.strftime('%b %d %Y')
-msg['From'] = 's079157@ap.be'
+msg['From'] = 'penpi.infsec@gmail.com'
 msg['To'] = to
 try :
         print("Telenet testen")
         smtpserver = smtplib.SMTP('uit.telenet.be', 25)
         smtpserver.ehlo()
-        smtpserver.sendmail('s079157@ap.be', [to], msg.as_string())
+        smtpserver.sendmail('penpi.infsec@gmail.com', [to], msg.as_string())
         smtpserver.quit()
         print("telenet gelukt")
 except:
@@ -34,11 +34,11 @@ except:
         print("telenet mislukt")
 try :
         print("ap starten");
-        msg['From'] = 's079157@ap.be'
-        msg['To'] = 's079157@ap.be'
+        msg['From'] = 'penpi.infsec@gmail.com'
+        msg['To'] = 'penpi.infsec@gmail.com'
         smtpserver = smtplib.SMTP('mailrelay.ap.be', 25)
         smtpserver.ehlo()
-        smtpserver.sendmail('s079157@ap.be', ['s079157@ap.be'], msg.as_string())
+        smtpserver.sendmail('penpi.infsec@gmail.com', ['penpi.infsec@gmail.com'], msg.as_string())
         smtpserver.quit()
         print("ap gelukt")
 except:
