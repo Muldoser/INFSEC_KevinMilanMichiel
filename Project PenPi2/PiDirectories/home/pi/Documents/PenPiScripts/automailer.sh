8a0a31a0233006b6a539a0112ba16bddfa6f7ef6
kevinvdm <s079154@ap.be>
2016-01-15 08:36:05 +0100

    automailer.sh corrected

--- Project PenPi2/PiDirectories/home/pi/Documents/PenPiScripts/automailer.sh
+++ Project PenPi2/PiDirectories/home/pi/Documents/PenPiScripts/automailer.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # Auto nmap mailing service
 
-#sudo nmap --open -O 192.168.0.* 192.168.1.*/24 > output.txt
-sudo nmap -T5 192.168.0.1 > output.txt
+sudo nmap --open -O 192.168.0.*/24 192.168.1.*/24 > output.txt
 cd ~/Documents/PenPiScripts/
 sudo python mailnmap.py
 
